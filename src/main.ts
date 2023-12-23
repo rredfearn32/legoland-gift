9a2f4060b29575d9a5065a5e95f85c3a5132f7bc
Robbie Redfearn <rredfearn32@gmail.com>
2023-12-23 19:28:47 +0100

    Create basic lego combo block with shatter effect

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import { Engine, Render, Runner, Bodies, Composite } from "matter-js";
+import {
+  Engine,
+  Render,
+  Runner,
+  Bodies,
+  Composite,
+  Body,
+  Sleeping,
+} from "matter-js";
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+const isSleeping = true;
+
+const colors = ["red", "blue", "green", "yellow", "white", "black"];
 
 // create an engine
 var engine = Engine.create();
@@ -7,15 +22,89 @@ var engine = Engine.create();
 var render = Render.create({
   element: document.querySelector("#matter") as HTMLInputElement,
   engine: engine,
+  options: {
+    height: screenHeight,
+    width: screenWidth,
+    background: "rgba(255,255,255,1)",
+    wireframes: false,
+  },
 });
 
-// create two boxes and a ground
-var boxA = Bodies.rectangle(400, 200, 80, 80);
-var boxB = Bodies.rectangle(450, 50, 80, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+const ground = Bodies.rectangle(
+  screenWidth / 2,
+  screenHeight - 1,
+  screenWidth,
+  100,
+  {
+    isStatic: true,
+  }
+);
+const ceiling = Bodies.rectangle(screenWidth / 2, -100, screenWidth, 100, {
+  isStatic: true,
+});
+const leftWall = Bodies.rectangle(0, screenHeight - 1, 1, screenHeight * 2, {
+  isStatic: true,
+});
+const rightWall = Bodies.rectangle(
+  screenWidth,
+  screenHeight - 1,
+  1,
+  screenHeight * 2,
+  {
+    isStatic: true,
+  }
+);
+
+const blocks: Body[] = [];
+
+const height = 50,
+  width = 100;
+
+let positionCountX = 0;
+
+for (let i = 0; i < 6; i++) {
+  let positionCountY = 0;
+  for (let j = 0; j < 10; j++) {
+    let x;
+
+    if (i < 3) {
+      x = screenWidth / 2 - width / 2 - positionCountX * width;
+    } else {
+      if (positionCountX === 3) {
+        positionCountX = 0;
+      }
+      x = screenWidth / 2 + width / 2 + positionCountX * width;
+    }
+
+    let y;
+
+    if (j < 5) {
+      y = screenHeight / 2 - height / 2 - positionCountY * height;
+    } else {
+      if (positionCountY === 5) {
+        positionCountY = 0;
+      }
+      y = screenHeight / 2 + height / 2 + positionCountY * height;
+    }
+
+    blocks.push(
+      Bodies.rectangle(x, y, width, height, {
+        isSleeping,
+        render: {
+          fillStyle: colors[Math.floor((Math.random() * 100) / 5)],
+          lineWidth: 0,
+        },
+      })
+    );
+
+    positionCountY++;
+  }
+
+  positionCountX++;
+}
 
 // add all of the bodies to the world
-Composite.add(engine.world, [boxA, boxB, ground]);
+Composite.add(engine.world, [...blocks, ground, ceiling, leftWall, rightWall]);
 
 // run the renderer
 Render.run(render);
@@ -25,3 +114,15 @@ var runner = Runner.create();
 
 // run the engine
 Runner.run(runner, engine);
+
+render.canvas.onclick = () => {
+  blocks.forEach((i) => {
+    Sleeping.set(i, false);
+    render.options.background = "rgba(255,255,255,0)";
+    const isNegative = Math.random() < 0.5;
+
+    const randomX = Math.random() * (isNegative ? -1 : 1);
+    const randomY = Math.random() * (isNegative ? -1 : 1);
+    i.force = { x: randomX / 5, y: randomY / 5 };
+  });
+};
